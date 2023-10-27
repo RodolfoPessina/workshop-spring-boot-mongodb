38735c603b14c2fbeb87ce9a3f7d48b9296ae284
Rodolfo Pessina <rodolfopessina@yahoo.com.br>
2023-10-26 22:02:16 -0300

    instanciacao da base de dados

---
+++ src/main/java/com/rodolfopessina/workshopmongo/config/Instantiation.java
@@ -0,0 +1,36 @@
+package com.rodolfopessina.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.rodolfopessina.workshopmongo.domain.User;
+import com.rodolfopessina.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+	
+	
+	
+	
+
+}
