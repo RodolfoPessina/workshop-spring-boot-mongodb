424a0602c098c7d08103520784c20fcb55fe69e1
Rodolfo Pessina <rodolfopessina@yahoo.com.br>
2023-10-30 23:26:53 -0300

    referenciando post usuario

--- src/main/java/com/rodolfopessina/workshopmongo/domain/User.java
+++ src/main/java/com/rodolfopessina/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.rodolfopessina.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
 	}
 
@@ -23,6 +29,14 @@ public class User implements Serializable {
 		this.name = name;
 		this.id = id;
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	public String getName() {
