3faad1c35f7475c0917f32f2d67f0330fdddfea0
Rodolfo Pessina <rodolfopessina@yahoo.com.br>
2023-10-30 23:40:20 -0300

    obtendo post por id

---
+++ src/main/java/com/rodolfopessina/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.rodolfopessina.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodolfopessina.workshopmongo.domain.Post;
+import com.rodolfopessina.workshopmongo.repository.PostRepository;
+import com.rodolfopessina.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
